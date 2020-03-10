ed37b9f3bce7570def8281ca2413d7ca8272fe08
Brooks J Rady <b.j.rady@gmail.com>
2020-03-10 18:33:28 +0000

    Ok

--- myo_ware.ino
+++ myo_ware.ino
@@ -1,9 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
+// the loop routine runs over and over again forever:
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float angle = sensorValue * (80.0 / 1023.0);
+  // print out the value you read:
+  Serial.println(angle);
+  Serial.print("Sensor reading ");
+  Serial.println(sensorValue);
+  myservo.write(angle);
+  
+  delay(50);
 }
